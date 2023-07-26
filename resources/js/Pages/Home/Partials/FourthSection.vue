a4f13653a735b317ac3cb0962d1c249e7bd6e864
Artem Serbin <49901978+itsserbin@users.noreply.github.com>
2023-07-26 11:58:34 +0300

    Update FourthSection.vue
    update map

--- resources/js/Pages/Home/Partials/FourthSection.vue
+++ resources/js/Pages/Home/Partials/FourthSection.vue
@@ -7,8 +7,7 @@
             <div class="text-4xl md:text-6xl font-[700] font-heading text-[#FFFFFF] ">
                 Ми знаходимось:
             </div>
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d20322.03238931802!2d30.478903924389645!3d50.45499375173574!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4ce59e3ab65a7%3A0x694b8565cab02eaf!2z0JfQvtC70L7RgtGL0LUg0LLQvtGA0L7RgtCw!5e0!3m2!1sru!2sua!4v1687552995503!5m2!1sru!2sua"
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1311.286163160524!2d24.7187983!3d48.9044872!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4730c15a199d316f%3A0x495293e210bec53e!2zOEEsINGD0LsuINCg0LXQsdC10YLQsCwgONCQLCDQmNCy0LDQvdC-LdCk0YDQsNC90LrQvtCy0YHQuiwg0JjQstCw0L3Qvi3QpNGA0LDQvdC60L7QstGB0LrQsNGPINC-0LHQu9Cw0YHRgtGMLCA3NjAwMA!5e0!3m2!1sru!2sua!4v1690361759758!5m2!1sru!2sua"
                 width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy"
                 class="rounded-lg"
                 referrerpolicy="no-referrer-when-downgrade"></iframe>
