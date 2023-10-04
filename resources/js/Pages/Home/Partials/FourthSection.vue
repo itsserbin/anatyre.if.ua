a7b68de8932efff01614d36fdfd6902f19dce6fe
Artem Serbin <49901978+itsserbin@users.noreply.github.com>
2023-10-04 20:28:02 +0300

    Update FourthSection.vue

--- resources/js/Pages/Home/Partials/FourthSection.vue
+++ resources/js/Pages/Home/Partials/FourthSection.vue
@@ -4,26 +4,30 @@
       <div class="text-4xl md:text-6xl font-[700] font-heading text-[#FFFFFF]">
         Ми знаходимось:
       </div>
-      <div @click="openGoogleMaps" style="cursor: pointer;">
-        <iframe
-          width="100%"
-          height="400"
-          frameborder="0"
-          style="border: 0"
-          src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2622.5742061312244!2d24.717349015676003!3d48.904451379291956!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDjCsDU0JzE2LjAiTiAyNMKwNDMnMTAuMyJF!5e0!3m2!1suk!2sua!4v1690529216651!5m2!1suk!2sua"
-          allowfullscreen=""
-          loading="lazy"
-        ></iframe>
-      </div>
+      <iframe
+        @click="openGoogleMaps"
+        src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2622.5742061312244!2d24.717349015676003!3d48.904451379291956!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDjCsDU0JzE2LjAiTiAyNMKwNDMnMTAuMyJF!5e0!3m2!1suk!2sua!4v1690529216651!5m2!1suk!2sua"
+        width="100%"
+        height="400"
+        style="border:0;"
+        allowfullscreen=""
+        loading="lazy"
+        class="rounded-lg"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
     </div>
   </section>
 </template>
 
-<script setup>
-const openGoogleMaps = () => {
-  window.open(
-    'https://www.google.com/maps?q=48.904451379291956,24.717349015676003&z=16&output=embed',
-    '_blank'
-  );
+<script>
+export default {
+  methods: {
+    openGoogleMaps() {
+      const lat = 48.904451379291956;
+      const lng = 24.717349015676003;
+      const url = `https://www.google.com/maps?q=${lat},${lng}&z=16&output=embed`;
+      window.open(url, "_blank");
+    },
+  },
 };
 </script>
